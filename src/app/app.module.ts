0de32289e733ff7ad2865116c6e9321caa3d39e4
Alejandro Montoya <alejandromontoyap@gmail.com>
2018-05-09 17:34:21 -0500

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
